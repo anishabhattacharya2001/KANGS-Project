db9199a7a3025a6950cc492569120f14a33f4ea4
Anisha <bhattacharyaanisha2001@gmail.com>
2022-11-29 13:45:05 -0500

    figure out backend

--- parktest.js
+++ parktest.js
@@ -7,7 +7,14 @@ let camera_stream = null;
 let media_recorder = null;
 let blobs_recorded = [];
 
-camera_button.addEventListener("click", async function () {
+function show(divId) {
+  $("#" + divId).show();
+}
+
+async function startClicked(){
+  show("control-div");
+  //document.getElementById("control-div").style.display = "block";
+  console.log("button should disappear");
   try {
     camera_stream = await navigator.mediaDevices.getUserMedia({
       video: true,
@@ -17,27 +24,34 @@ camera_button.addEventListener("click", async function () {
     alert(error.message);
     return;
   }
-
+  camera_button.style.display = "none";
   video.srcObject = camera_stream;
-  camera_button.style.display="block";
+  // camera_button.style.display = "block";
   video.style.display = "block";
   start_button.style.display = "block";
-});
+
+}
+// camera_button.addEventListener("click", async function () {
+//   show("control-div");
+//   //document.getElementById("control-div").style.display = "block";
+//   console.log("button should disappear");
+//   try {
+//     camera_stream = await navigator.mediaDevices.getUserMedia({
+//       video: true,
+//       audio: true,
+//     });
+//   } catch (error) {
+//     alert(error.message);
+//     return;
+//   }
+//   camera_button.style.display = "none";
+//   video.srcObject = camera_stream;
+//   // camera_button.style.display = "block";
+//   video.style.display = "block";
+//   start_button.style.display = "block";
+// });
 
 start_button.addEventListener("click", function () {
-    try {
-        camera_stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-          audio: true,
-        });
-      } catch (error) {
-        alert(error.message);
-        return;
-      }
-    
-      video.srcObject = camera_stream;
-      video.style.display = "block";
-      start_button.style.display = "block";
   media_recorder = new MediaRecorder(camera_stream, { mimeType: "video/webm" });
   start_button.style.display = "none";
 
@@ -62,4 +76,3 @@ start_button.addEventListener("click", function () {
 stop_button.addEventListener("click", function () {
   media_recorder.stop();
 });
-
