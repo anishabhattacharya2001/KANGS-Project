ac665f3537c43451d2f4754862d0558a326b5649
Anisha <bhattacharyaanisha2001@gmail.com>
2022-12-07 17:44:42 -0500

    should work

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-# from flask import Flask
-# app=Flask(__name__)
+# from flask import Flask, render_template
+# app = Flask(__name__)
 
-# @app.route(‘/’)
-# def hello_world():
-#    return 'Hello World'
-
+# @app.route('/')
+# def index():
+#    return render_template('index.html')
 
 # if __name__ == '__main__':
 #    app.run(debug=True)
--- parktest.js
+++ parktest.js
@@ -7,14 +7,8 @@ let camera_stream = null;
 let media_recorder = null;
 let blobs_recorded = [];
 
-function show(divId) {
-  $("#" + divId).show();
-}
-
 camera_button.addEventListener("click", async function () {
-  show("control-div");
-  //document.getElementById("control-div").style.display = "block";
-  console.log("button should disappear");
+  camera_button.style.display="none"; 
   try {
     camera_stream = await navigator.mediaDevices.getUserMedia({
       video: true,
@@ -24,14 +18,27 @@ camera_button.addEventListener("click", async function () {
     alert(error.message);
     return;
   }
-  camera_button.style.display = "none";
+
   video.srcObject = camera_stream;
-  // camera_button.style.display = "block";
+  camera_button.style.display="block";
   video.style.display = "block";
   start_button.style.display = "block";
 });
 
-start_button.addEventListener("click", function () {
+start_button.addEventListener("click", async function () {
+    try {
+        camera_stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+      } catch (error) {
+        alert(error.message);
+        return;
+      }
+    
+      video.srcObject = camera_stream;
+      video.style.display = "block";
+      start_button.style.display = "block";
   media_recorder = new MediaRecorder(camera_stream, { mimeType: "video/webm" });
   start_button.style.display = "none";
 
@@ -56,3 +63,4 @@ start_button.addEventListener("click", function () {
 stop_button.addEventListener("click", function () {
   media_recorder.stop();
 });
+
